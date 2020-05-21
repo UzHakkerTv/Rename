6e521a39f41d71c8220a0bc5067ef2179650dbcb
UzHakkerTv <62137063+UzHakkerTv@users.noreply.github.com>
2020-05-21 21:50:04 +0500

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,46 @@
+﻿class Translation(object):
+    START_TEXT = """✋Salom,
+Bu Telegram Rename Bot!
+
+<b>Iltimos, menga biron bir Telegram faylini yuboring va ushbu faylga /rename New Name.mkv-ga javob bering</b>
+
+/help yordam uchun"""
+    RENAME_403_ERR = "❌Kechirasiz. Ushbu faylning nomini o'zgartirishga ruxsat yo'q."
+    ABS_TEXT = "Iltimos Xudbin Bo'lmang"
+    UPGRADE_TEXT = "<b>👉 </b>  /help yordam uchun"
+    DOWNLOAD_START = "Yuklab Olishga harakat qilmoqda"
+    UPLOAD_START = "Yuklashga harakat qilmoqda"
+    RCHD_TG_API_LIMIT = "{} Soniya ichida yuklab olindi.\nAniqlangan fayl hajmi: {}\nKechirasiz. Ammo, men Telegram API cheklovlari tufayli 1.5 Gb dan katta fayllarni yuklay olmayman."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Mendan Foydalanayotganingiz uchun Rahmat🤓."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "{} Soniya ichida yuklab olindi.\n{} Soniya ichida yuklandi."
+    NOT_AUTH_USER_TEXT = "Iltimos / upgrade obunangizni oshiring."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Aniqlangan fayl hajmi: {}. Bepul foydalanuvchilar faqat yuklashlari mumkin: {}\nIltimos, obunangizni /upgrade qiling.\nAgar bu xato deb o'ylasangiz, iltimos murojaat qiling<a href='https://telegram.me/PHP_Secrets'>PHP_Secrets</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Shaxsiy fayl eskizi saqlandi. Ushbu rasm faylda ishlatiladi."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Xos eskiz muvaffaqiyatli o'chirildi."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media muvaffaqiyatli o'chirildi."
+    SAVED_RECVD_DOC_FILE = "Hujjat muvaffaqiyatli yuklandi."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Shaxsiy eskiz topilmadi."
+    USER_ADDED_TO_DB = "Foydalanuvchi <a href='tg://user?id={}'>{}</a> qo'shildi {} ga {}."
+    HELP_USER = """Salom ✋
+    
+1. Menga biron bir Telegram faylini yuboring.
+2. Ushbu xabarga /rename new name.extension ga javob bering.
+   
+<a href="https://youtu.be/c/OtabekChoriyev">📡Kanalimiz</a>
+
+--------
+
+💻Dasturchi: @RedFoc
+📡Kanallarimiz @PHP_Secrets , @UniversalVlog, @EHMUZ"""
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Telegram mediaga /rename New Name.extension-ga maxsus eskiz yordami bilan javob bering .."
+    ABUSIVE_USERS = "Ushbu botni ishlatishingizga ruxsat yo'q. Agar bu xato deb o'ylasangiz, ushbu cheklovni olib tashlash uchun /me ni tekshiring."
+    FREE_USER_LIMIT_Q_SZE = """Ishlov berib bo‘lmadi.
+Bepul foydalanuvchilar 30 daqiqada atigi bitta so'rov.
+/upgrade yoki 1800 soniyadan keyin urinib ko'ring."""
+    IFLONG_FILE_NAME = """Telegram tomonidan ruxsat etilgan fayl nomining chegarasi {alimit} belgilar.
+Ushbu fayl nomi mavjud {num} belgilar.
+
+<b>Telegram fayl nomida insholarga yo'l qo'yilmaydi!</b>
+©️ <code>@PHP_Secrets</code>
+Iltimos, fayl nomingizni qisqartiring va qayta urinib ko'ring!"""
